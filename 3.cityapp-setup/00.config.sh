b28cf56b9f1e5103bad2aa714bcf7b5b05e7a924
huydd79 <86530496+huydd79@users.noreply.github.com>
2022-11-21 00:35:14 +0700

    Update 00.config.sh

--- 3.cityapp-setup/00.config.sh
+++ 3.cityapp-setup/00.config.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-
+source ../2.conjur-setup/00.config.sh
 #Change your configuration and set READY=true when done
 READY=false
 
-#IP addresses of conjur and crc VM
-LAB_IP=172.16.100.109
-LAB_DOMAIN=demo.local
-LAB_CONJUR_ACCOUNT=DEMO
