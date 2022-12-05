2d3fdc71c2060a574ed9388a68a3c2ea90776251
huydd79 <86530496+huydd79@users.noreply.github.com>
2022-12-05 11:20:31 +0700

    Update 04.installing-k8s-dashboard.sh
    Optimising script to delete old deployments before run

--- 1.k8s-setup/04.installing-k8s-dashboard.sh
+++ 1.k8s-setup/04.installing-k8s-dashboard.sh
@@ -1,6 +1,35 @@
 #/bin/sh
 
 set -x
+APP_NAME="dashboard-metrics-scraper"
+kubectl -n kubernetes-dashboard get deployments | grep -q $APP_NAME
+if [ $? -eq 0 ]; then
+    kubectl -n kubernetes-dashboard delete deployment $APP_NAME
+    ret=0
+    until [ $ret -ne 0 ]
+    do
+        kubectl -n kubernetes-dashboard get deployments | grep -q $APP_NAME
+        ret=$?
+        echo "Waiting deployment is deleted..."
+        sleep 1
+    done
+
+fi
+
+APP_NAME="kubernetes-dashboard"
+kubectl -n kubernetes-dashboard get deployments | grep -q $APP_NAME
+if [ $? -eq 0 ]; then
+    kubectl -n kubernetes-dashboard delete deployment $APP_NAME
+    ret=0
+    until [ $ret -ne 0 ]
+    do
+        kubectl -n kubernetes-dashboard get deployments | grep -q $APP_NAME
+        ret=$?
+        echo "Waiting deployment is deleted..."
+        sleep 1
+    done
+
+fi
 
 kubectl apply -f yaml/kube-dashboard.yaml
 
