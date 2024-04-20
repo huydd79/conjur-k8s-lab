5dfd77649ea7951e983cacde70bd5d905ef4e996
Huy Do <86530496+huydd79@users.noreply.github.com>
2024-04-20 15:13:51 +0700

    Update 00.config.sh

--- 2.conjur-setup/00.config.sh
+++ 2.conjur-setup/00.config.sh
@@ -4,15 +4,15 @@
 READY=false
 
 #IP addresses of conjur and crc VM
-CONJUR_IP=172.16.100.109
+CONJUR_IP=172.16.100.15
 LAB_IP=$CONJUR_IP
 LAB_DOMAIN=demo.local
 LAB_CONJUR_ADMIN_PW=ChangeMe123!
 LAB_CONJUR_ACCOUNT=DEMO
 #Path to folder with all docker images
 UPLOAD_DIR=/opt/lab/setup_files
-conjur_appliance_file=conjur-appliance-Rls-12.7.tar.gz
-conjur_version=12.7.0.1
+conjur_appliance_file=conjur-appliance-Rls-v13.2.0.tar.gz
+conjur_version=13.2.2
 #Conjur container name
-node_name=conjur1
+node_name=conjur
 
