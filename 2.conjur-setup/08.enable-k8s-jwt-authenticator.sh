2dac69fe2ebde69b4f0876138725e2556d9098e9
Huy Do <86530496+huydd79@users.noreply.github.com>
2024-04-20 15:19:12 +0700

    Update 08.enable-k8s-jwt-authenticator.sh

--- 2.conjur-setup/08.enable-k8s-jwt-authenticator.sh
+++ 2.conjur-setup/08.enable-k8s-jwt-authenticator.sh
@@ -9,7 +9,8 @@ fi
 set -x
 conjur -d policy load -f ./policies/authn-jwt-k8s.yaml -b root
 
-podman exec $node_name chpst -u conjur conjur-plugin-service possum rake authn_k8s:ca_init["conjur/authn-jwt/k8s"]
+# TODO: check this command to see if can be removed:
+#podman exec $node_name chpst -u conjur conjur-plugin-service possum rake authn_k8s:ca_init["conjur/authn-jwt/k8s"]
 podman exec -it $node_name sh -c 'grep -q "authn,authn-jwt/k8s" /opt/conjur/etc/conjur.conf || echo "CONJUR_AUTHENTICATORS=\"authn,authn-jwt/k8s\"\n">>/opt/conjur/etc/conjur.conf'
 podman exec $node_name sv restart conjur
 set +x
