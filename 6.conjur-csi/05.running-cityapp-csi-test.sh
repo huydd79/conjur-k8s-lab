c3c60adbd7ffb0ff0e9bd2e9d8d8b6ef7cf33329
Huy Do <86530496+huydd79@users.noreply.github.com>
2024-07-01 15:29:42 +0700

    Update 05.running-cityapp-csi-test.sh

--- 6.conjur-csi/05.running-cityapp-csi-test.sh
+++ 6.conjur-csi/05.running-cityapp-csi-test.sh
@@ -1,6 +1,13 @@
 #/bin/sh
 
+if [[ "$READY" != true ]]; then
+    echo "Your configuration are not ready. Set READY=true in 00.config.sh when you are done"
+    exit
+fi
+
 APP_NAME="cityapp-csi"
+YML_FILE="yaml/05.$APP_NAME.yaml"
+YML_TEMP="/tmp/05.$APP_NAME.yaml"
 
 set -x
 kubectl get namespace | grep -q cityapp || kubectl create namespace cityapp
@@ -18,7 +25,13 @@ if [ $? -eq 0 ]; then
     
 fi
 
+#Prepare manifest
+cp $YML_FILE $YML_TEMP
+sed -i "s/{LAB_IP}/$LAB_IP/g" $YML_TEMP
+sed -i "s/{LAB_DOMAIN}/$LAB_DOMAIN/g" $YML_TEMP
 
-kubectl -n cityapp apply -f ./yaml/05.$APP_NAME.yaml
+kubectl -n cityapp apply -f $YML_TEMP
+
+rm $YML_TEMP
 
 set +x
